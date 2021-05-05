1549a12c426c7d7d48498fd0aa62f77b074d528e
Jeff Baxter <jeffbaxter@hatfieldmedia.com>
2021-05-05 15:28:18 -0400

    adjustments for URL to be set to variable

--- hmscraper/hmscraper/spiders/hm_blog.py
+++ hmscraper/hmscraper/spiders/hm_blog.py
@@ -2,17 +2,22 @@ import scrapy
 import json
 from scrapy import signals
 
+# URL here
+base_url = "https://aac.hatfield.marketing"
+
+base_url = base_url.strip("/")
+
 # Storing urls in the set, then exporting at the end
 blog_urls = set()
 
 class HmblogSpider(scrapy.Spider):
 
     name = 'hmblog_twill'
-    start_urls = ['https://aac.hatfield.marketing/blog?all-page=1']
+    start_urls = [f'{base_url}/blog?all-page=1']
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Authorization":"null null",
-        "Referer": "https://aac.hatfield.marketing/blog",
+        "Referer": f'{base_url}/blog',
         "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
         "sec-ch-ua-mobile": "?0",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
@@ -21,7 +26,7 @@ class HmblogSpider(scrapy.Spider):
 
     # Gets API URL, then goes to parse API. 
     def parse(self, response):
-        url = "https://aac.hatfield.marketing/api/posts?blog%5B%5D=1&count=6&locale=en&order-by=publish_start_date"
+        url = f'{base_url}/api/posts?blog%5B%5D=1&count=6&locale=en&order-by=publish_start_date'
 
         request = scrapy.Request(url, callback=self.parse_api, headers=self.headers)
         
@@ -38,7 +43,7 @@ class HmblogSpider(scrapy.Spider):
 
         # Extract blog_urls from each blog that's loaded
         for blog in blog_data:
-            blog_url = "https://aac.hatfield.marketing" + blog["full_slug"]
+            blog_url = base_url + blog["full_slug"]
             blog_urls.add(str(blog_url))
 
         # If the blog page has more content to load
