2f2b97b9e270e2dc29a7980e5cf2c1ef3faadb55
Jeff <jeffbaxter@hatfieldmedia.com>
2021-06-28 17:04:11 -0400

    removed test.py

--- hmscraper/hmscraper/spiders/test.py
+++
@@ -1,6 +0,0 @@
-# URL here
-base_url = "https://raque.hatfield.marketing"
-base_url = base_url.strip("/")
-check_url = base_url.replace("http://", '').replace("https://", '').split("/")[0]
-
-print(check_url)
