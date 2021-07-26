b5813db10e0db32d2396a9823dc5be663c68fed5
Jeff Baxter <jeffbaxter@hatfieldmedia.com>
2021-07-26 15:14:50 -0400

    file got changed on merge, adjusting back to previous

--- hmscraper/hmscraper/spiders/hm_standard.py
+++ hmscraper/hmscraper/spiders/hm_standard.py
@@ -1,162 +1,142 @@
 import scrapy
 from scrapy import signals
-from scrapy.http import headers
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy_splash import SplashRequest
+
 # URL here
-base_url = "https://raque.hatfield.marketing/"
+base_url = "http://aac.hatfield.marketing/"
 base_url = base_url.strip("/")
+
+
 check_url = base_url.replace("http://", '').replace("https://", '').split("/")[0]
-
-
 
 # Storing urls for pages we've found to dump into a text file
 url_set = set()
+
 links = []
 pages = []
+
 lorem_string = "Lorem ipsum dolor amet consectetur adipiscing elit eiusmod tempor incididunt labore dolore magna aliqua enim minim veniam quis nostrud exercitation ullamco laboris nisi aliquip commodo consequat Duis aute irure dolor reprehenderit voluptate velit esse cillum dolore fugiat nulla pariatur Excepteur sint occaecat cupidatat proident culpa officia deserunt mollit laborum".split()
+
 lorem = set()
 
 for w in lorem_string:
     lorem.add(" " + w + " ")
+
 # Adding 'lorem ' incase text starts with lorem
 lorem.add("lorem ")
-
-# Storing urls with lorem ipsum
 lorem_url_set = set()
 
-
 class HMScraper(scrapy.Spider):
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-        "sec-ch-ua-mobile": "?0",
-    }
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
         spider = super(HMScraper, cls).from_crawler(crawler, *args, **kwargs)
         crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
         return spider
-    
+
     name = "standard"
     start_urls = []
     start_urls.append(base_url)
     allowed_domains = [check_url]
     deny = ["r/^mailto:/", "/^tel:/"]
-    # rules = [
-    #     Rule(LinkExtractor(), callback='parse_data', follow=True),
-    # ]
-    
-    def parse(self, response):
 
+    rules = [
+        Rule(LinkExtractor(), callback='parse_data', follow=True),
+    ]
+
+    def parse(self, response):
         for link in response.css('a::attr(href)').getall():
 
+            page_response_code = response.status
             # If the link has mailto or tel, don't process since this will fail. Call page_dump_null
             if "mailto:" in link or "tel:" in link:
-                yield from self.page_dump_null(response.request.url, link, "Mailto/Tel", "N/A")
+                yield from self.page_dump_null(response.urljoin(link), response.status, link, "Mailto/Tel")
+
             else:
                 # Send all links to parse_data function
-                yield SplashRequest(response.urljoin(link), callback=self.parse_data, headers=self.headers, meta={'original_url': link})
-    
+                yield SplashRequest(response.urljoin(link), callback=self.parse_data, args={'wait': 0.5}, meta={'original_url': link, 'original_url_response_code': page_response_code})
+
     def parse_data(self, response):
 
-        page_url = response.meta['original_url']
-
-        # Cleaning up page URL since Splash adds the port at the end of the URL
-        if page_url.startswith('/'):
-            page_url = base_url+page_url
-
+        page_response_code = response.status
+        page_url = response.url
         links = response.css('a::attr(href)').getall()
 
         link_text = response.xpath('//div[@id="app"]//text()').extract()
         link_string = str(link_text)
 
+
         for link in links:
 
-            page_url = page_url.replace(":443","").replace(":80","")
-            # If the link has mailto or tel, don't process since this will fail. Call page_dump_null
+            # If mailto or tel in link
+            # else if the link is local
+            # else - the URL is an external link
             if "mailto:" in link or "tel:" in link:
-                    link_type = "Mailto/Tel"
-                    yield from self.page_dump_null(page_url, link, link_type, "N/A")
-            else:                    
-                
-                # Using regular scrapy just to get the response codes for the urls
-                yield scrapy.Request(response.urljoin(link), callback=self.sub_url, dont_filter=True, headers=self.headers, meta={'original_url': page_url})
+                link_type = "Mailto/Tel"
+                yield from self.page_dump_null(page_url, page_response_code, link, link_type)
 
-            
-            # Lorem Ipsum Checker
-            for l in lorem:
-                if l in link_string:
-                    print(f'Found {l} in {page_url}')
-                    lorem_url_set.add(page_url)
-                    break
-    
-    # This is called for broken links, tel/mailto links, etc...
-    def page_dump_null(self, page_url, link, link_type, link_response):
+            elif check_url in link or link.startswith("/"):
 
-        # SplashRequest appends the port number at the end on original request. Removing these if detected.
-        page_url = page_url.replace(":443","").replace(":80","").strip("/")
-    
-        yield {
-            "Page": page_url,
-            "Link": link,
-            "Link Type": link_type,
-            "Link Response": link_response
-        }
-    
-    # Checks urls on page, calls page_dump_null() for pages that report bad response codes. 
-    def sub_url(self, response):
-
-        page_url = response.meta['original_url']
-
-        response_code = response.status
-        link = response.url
-
-        if 200 <= response_code and response_code <= 299:
-
-            if check_url in link or link.startswith("/"):
                 link_type = "Local"
-                
-                # Adding local URL to URL set, which gets dumped into a text file at the end.
-                # This is used to run local links through the additinoal scripts
-                url_set.add(str(link))
 
                 if link.startswith("/"):
                     link = base_url+link
 
+                # Adding local URL to URL set, which gets dumped into a text file at the end.
+                # This is used to run local links through the additinoal scripts
+                url_set.add(str(link))
+
             else:
                 link_type = "External"
-            
+
             # Cleaning up page URL since Splash adds the port at the end of the URL
             if page_url.startswith('/'):
-                page_url = base_url+page_url
+                page_url = base_url+response.meta['original_url']
             
-            page_url = page_url.replace(":443","").replace(":80","")
-
+            page_url = page_url.replace(":443","").replace(":80","").strip("/")
+            
             # Dumping output
             yield {
                 "Page": page_url,
+                "Page Response": page_response_code,
                 "Link": link,
                 "Link Type": link_type,
                 "Link Response": response.status,
             }
-        else:
-            yield from self.page_dump_null(page_url, response.url, 'Link', response.status)
+        
+        # Lorem Ipsum Checker
+        for l in lorem:
+            if l in link_string:
+                print(f'Found {l} in {page_url}')
+                lorem_url_set.add(page_url)
+                break
+
+    def page_dump_null(self, page_url, page_response_code, link, link_type):
+
+        # SplashRequest appends the port number at the end on original request. Removing these if detected.
+        page_url = page_url.replace(":443","").replace(":80","").strip("/")
+
+        yield {
+            "Page": page_url,
+            "Page Response": page_response_code,
+            "Link": link,
+            "Link Type": link_type,
+            "Link Response": "N/A",
+        }
 
     # When the spider is completed, all local urls are dumped to a txt file.
     def spider_closed(self, spider):
-        
+
         # File name
         name = check_url.replace("http://", '').replace("https://", '').split("/")[0].split(".")
-        
+
         # Writing local URLs to txt file as name of site
         f = open(name[0] + "-links.txt", 'w+')
         f.write('\n'.join(map(str, url_set)))
         f.close()
-        
+
         # Conditional for URLs that contain lorem ipsum.
         if lorem_url_set:
             lf = open(name[0] + "-lorem-check.txt", 'w+')
