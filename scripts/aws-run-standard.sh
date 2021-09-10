b9b4da3e4198621e779dde2cab38d8cf0ff4c15a
Jeff Baxter <jeffbaxter@hatfieldmedia.com>
2021-09-10 10:40:15 -0400

    adjusted comments in shell script

--- scripts/aws-run-standard.sh
+++ scripts/aws-run-standard.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# General Summary:
 #For each url in the urls.json, run the checker and download the results
 
+# Changing to the directory for the script, activate the virtual environment, and cd to hmscraper/hmscraper
 cd /home/ssm-user/scripts/py-link-check
 
 . ./venv/bin/activate
 
 cd hmscraper/hmscraper/
 
+# Copy down the urls.json file here
 aws s3 cp s3://daily-link-check/urls.json /home/ssm-user/scripts/py-link-check/hmscraper/hmscraper/urls.json
 
 # Starts Docker
@@ -16,8 +19,10 @@ sudo systemctl start docker
 # Starts Container
 sudo docker run --name splash -d -p 8050:8050 --rm scrapinghub/splash
 
+# variable used to get the current date for for the CSV file
 NOW=$(date +"%m-%d-%y")
 
+# Variable used to get the month, this is used to create the month folder in S3
 MONTH=$(date +"%b")
 
 # Running command to check to see if the month folder has been created
@@ -31,15 +36,15 @@ if  [[ $? -ne 0 ]]; then
     aws s3api put-object --bucket daily-link-check --key "$MONTH"/lorem/
 fi
 
-# Now loop through the urls and run the script
+# Now we loop through the urls.json file and run the link-check script. 
 jq -c -r '.urls[]' urls.json | while read i; do
     name=$(echo $i |  awk -F[/:] '{print $4}' | cut -f1 -d".")
     scrapy crawl aws-standard -a url="$i" -O ./reports/"$NOW"_"$name".csv
 
-    # Setting variable for API endpoint to test for Twill sites. Removes trailing slash if it's present
+    # Variables used for  checking API endpoint for twill sites. If the site is Twill site, then the blogs get checked via a separate script. Removes trailing slash
     VAR=$(echo $i | sed -e 's#/$##')
 
-    # Variable for the API enpoints
+    # Variable for the API enpoints. This checks via a curl request. 
     API_ENDPOINTS=$(curl -L $VAR/api/posts)
 
     # Checking to see if the site is a twill site. If so, we want to run the blog check on this blogs as well. 
